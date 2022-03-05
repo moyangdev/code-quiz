686fdf4a2c0494b6e4283ace772d05a005bf145f
Mo Yang <moyang313@gmail.com>
2022-03-05 17:02:49 -0500

    hide starter page on button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var container = document.querySelector('.container');
-
 var timerEl = document.getElementById('countdown');
+var startEl = document.getElementById('quizStart');
+var startScreenEl = document.getElementById("start-screen");
 
 var questions = [
     {
@@ -30,22 +31,30 @@ var questions = [
     }
 ];
 
-document.getElementById("quizStart").addEventListener("click", function(){
-// Timer that counts down from 75
-var timeLeft = 75;
-// Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
-var timeInterval = setInterval(function () {
-// As long as the `timeLeft` is greater than 1
-if (timeLeft > 1) {
-    // Set the `textContent` of `timerEl` to show the remaining seconds
-    timerEl.textContent = 'Time: ' + timeLeft;
-    // Decrement `timeLeft` by 1
-    timeLeft--;
-} else {
-    // Once `timeLeft` gets to 0, set `timerEl` to Time is up
-    timerEl.textContent = 'Time is up!';
-    // Use `clearInterval()` to stop the timer
-    clearInterval(timeInterval);
-}
-}, 1000);
-});
+function quizTimer(){
+    // Timer that counts down from 75
+    var timeLeft = 75;
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    var timeInterval = setInterval(function () {
+    // As long as the `timeLeft` is greater than 1
+    if (timeLeft > 1) {
+        // Set the `textContent` of `timerEl` to show the remaining seconds
+        timerEl.textContent = 'Time: ' + timeLeft;
+        // Decrement `timeLeft` by 1
+        timeLeft--;
+    } else {
+        // Once `timeLeft` gets to 0, set `timerEl` to Time is up
+        timerEl.textContent = 'Time is up!';
+        // Use `clearInterval()` to stop the timer
+        clearInterval(timeInterval);
+    }
+    }, 1000);
+    }
+    
+    function startQuiz(){
+        //start timer
+        quizTimer();
+        startScreenEl.setAttribute("class", "hide");
+    }
+
+    startEl.onclick = startQuiz;
