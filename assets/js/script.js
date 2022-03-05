b55fb7d03d3d21bdad57b7f7d0f7ac4f5231d059
Mo Yang <moyang313@gmail.com>
2022-03-05 15:22:59 -0500

    update logic to have timer start on button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,9 @@ var container = document.querySelector(".container");
 
 var timerEl = document.getElementById('countdown');
 
-// Timer that counts down from 5
-function countdown() {
+document.getElementById("quizStart").addEventListener("click", function(){
+// Timer that counts down from 75
 var timeLeft = 75;
-
 // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
 var timeInterval = setInterval(function () {
 // As long as the `timeLeft` is greater than 1
@@ -15,14 +14,10 @@ if (timeLeft > 1) {
     // Decrement `timeLeft` by 1
     timeLeft--;
 } else {
-    // Once `timeLeft` gets to 0, set `timerEl` to an empty string
-    timerEl.textContent = '';
+    // Once `timeLeft` gets to 0, set `timerEl` to Time is up
+    timerEl.textContent = 'Time is up!';
     // Use `clearInterval()` to stop the timer
     clearInterval(timeInterval);
-    // Call the `displayMessage()` function
-    displayMessage();
 }
 }, 1000);
-}
-
-countdown();
+});
